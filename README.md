d33aaf7aa8fe01185babf31e2eee170fc900d5b5
kelly elvis <kellyelvisn@gmail.com>
2025-01-26 23:56:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Akan Name Generator
+
+This project is created in order to generate your Akan name based on your date of birth and gender. Akan names are derived from the Akan people of Ghana, who have a rich tradition of naming children according to the day of the week they were born.
+
+## Creator
+
+This project was created by **Kelly Elvis**.
+
+## Features
+1. Input your date of birth and gender.
+2. Receive your Akan name based on Ghanaian naming system.
+
+## How to Use
+1. Enter your date of birth in the form provided.
+2. Specify your gender in the input boxes provided.
+3. View your generated Akan name according to your input.
+
+## About Akan Names
+Akan names are culturally significant and reflect the day of the week a person was born. Each day is associated with specific male and female names.
+
+For example:
+- **Monday**: Kwandwo (Male), Adowa (Female)
+- **Friday**: Kofi (Male), Afua (Female)
+
+
+## License
+This project is open-source and available for personal and educational use.
+
+Just use akan name generator to get your Akan name.
---
+++ test.js
@@ -0,0 +1,56 @@
+document.getElementById('birthdayNamerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    const day = document.getElementById('day').value;
+    const month = document.getElementById('month').value;
+    const year = document.getElementById('year').value;
+    const gender = document.getElementById('gender').value;
+
+    
+    const date = new Date(`${year}-${month}-${day}`);
+    const dayOfWeek = date.getDay(); 
+
+    // naming
+    let name ="";
+    
+
+if (gender === "male") {
+    if (dayOfWeek === 1) {
+      name = "Kwadwo";
+    } else if (dayOfWeek === 2) {
+      name = "Kwabena";
+    } else if (dayOfWeek === 3) {
+        name = "Kwaku";
+    } else if (dayOfWeek === 4) {
+            name = "Yaw";
+     } else if (dayOfWeek === 5) {
+            name = "Kofi";
+        } else if (dayOfWeek === 6) {
+            name = "Kwame";
+        } else if (dayOfWeek === 0) {
+            name = "Kwasi";
+        }
+    } else if (gender === "female") {
+    if (dayOfWeek === 1) {
+      name = "Adowa";
+    } else if (dayOfWeek === 2) {
+      name = "Abenaa";
+    } else if (dayOfWeek === 3) {
+        name = "Akua";
+      } else if (dayOfWeek === 4) {
+        name = "Yaa";
+      } else if (dayOfWeek === 5) {
+        name = "Afua";
+      } else if (dayOfWeek === 6) {
+        name = "Ama";
+      } else if (dayOfWeek === 0) {
+        name = "Akosua";
+      }
+}
+const akaneName = `${name}`;
+
+    // Display 
+    const resultDiv = document.getElementById('generatedName');
+    resultDiv.innerHTML = `<p>Your Akane birthday name is: <strong>${akaneName}</strong></p>`;
+  });
